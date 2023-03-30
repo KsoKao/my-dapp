6c966aec50a802bb188c87a930f9ffb574684f06
KsoKaa <kami38720@gmail.com>
2023-03-30 16:38:03 +0900

    card

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,59 @@ function App() {
       </header>
       <main>
         {/* Introdice */}
-        <div className="bg-purple-100 max-w-screen-xl ma-auto h-96 flex justify-center items-center">
+        <div className="bg-purple-100 max-w-screen-x1 ma-auto h-96 flex justify-center items-center">
           여기에 나를 소개하는 내용을 적으면 됩니다.
         </div>
+        <ul className="bg-indigo-100 max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-8 py-8">
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-tr-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-tr-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-tr-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-tr-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-tr-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-tr-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+        </ul>
       </main>
     </div>
   );
